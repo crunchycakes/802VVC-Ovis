5905ca44155e421db6805b3d49902b8f86a5a410
crunchycakes <85715474+crunchycakes@users.noreply.github.com>
2025-04-06 04:00:00 -0600

    proj (late commit)

---
+++ backend/app.py
@@ -0,0 +1,42 @@
+import csv
+import os
+from flask import Flask, abort, send_from_directory, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+imgdir = os.path.join(app.root_path, 'imgs')
+
+@app.route('/api/message')
+def get_message():
+    return jsonify({'message': imgdir})
+
+@app.route('/api/chart-data')
+def get_chart_data():
+    data = []
+    with open("results.csv", newline='') as csvf:
+        reader = csv.DictReader(csvf)
+        for row in reader:
+            data.append({
+                'qp': int(row['quantization parameter']),
+                'bytes': int(row['bytes']),
+                'PSNR': float(row['PSNR']),
+                'seconds': int(row['seconds'])
+            })
+    return jsonify(data)
+
+@app.route('/api/image/<filename>')
+def get_image(filename):
+    
+    if not filename.endswith(('.jpg', '.png', '.jpeg', '.gif')):
+        abort(400, description="Invalid file type")
+    
+    file_path = os.path.join(imgdir, filename)
+    if not os.path.isfile(file_path):
+        abort(404, description="Image not found")
+    
+    return send_from_directory(imgdir, filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ frontend/src/App.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+
+const allMets = ["bytes", "PSNR", "seconds"]
+
+function App() {
+  const [message, setMessage] = useState('')
+  const [chartData, setChartData] = useState([]);
+  const [hoveredPoint, setHoveredPoint] = useState(null);
+  const [selectedMet, selectMet] = useState('PSNR');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/chart-data')
+      .then(res => setChartData(res.data))
+      .catch(err => console.error(err))
+      axios.get('http://localhost:5000/api/message')
+      .then(res => setMessage(res.data))
+      .catch(err => console.error(err))
+  }, []);
+
+  const colorway = (key) => {
+    const colors = {
+      bytes: '#b30000',
+      PSNR: '#5900de',
+      seconds: '#e0e0e0'
+    }
+    return colors[key];
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl mb-4">Quantization Curve</h1>
+
+      <div className="flex gap-4 mb-4">
+        {allMets.map((metric) => (
+          <label key={metric} className="flex items-center gap-2">
+            <input
+              type="radio"
+              name="metric"
+              value={metric}
+              checked={selectedMet === metric}
+              onChange={() => selectMet(metric)}
+            />
+            {metric}
+          </label>
+        ))}
+      </div>
+
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart data={chartData}
+          onMouseMove={(state) => {
+            if (state?.activePayload?.[0]) {
+              setHoveredPoint(state.activePayload[0].payload);
+            }
+          }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="qp" label={{ value: 'Quantization Parameter', position: 'insideBottom', offset: -5 }}/>
+          <YAxis label={{ value: 'Time to encode (seconds)', angle: -90, position: 'insideLeft'}}/>
+          <Tooltip />
+          <Line type="monotone" dataKey={selectedMet} stroke={colorway(selectedMet)} activeDot={{ r: 8 }} />
+          
+        </LineChart>
+      </ResponsiveContainer>
+      <img src={hoveredPoint ? `http://localhost:5000/api/image/imgqp${hoveredPoint.qp}.png` : 'http://localhost:5000/api/image/imgqp0.png'}
+        className='flex mt-4 rounded shadow'
+        alt="Zoomable"
+      />
+    </div>
+  )
+}
+
+export default App
